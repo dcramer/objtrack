27c202e57beefcac5556cebe344218f12407aa53
David Cramer <dcramer@gmail.com>
2009-01-27 22:55:36 +0000

    Missing import in manager

--- django_object_view_tracking/manager.py
+++ django_object_view_tracking/manager.py
@@ -1,5 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
-
+from django.db import models
 from django_object_view_tracking.constants import *
 
 class ObjectTrackerHandler(object):
