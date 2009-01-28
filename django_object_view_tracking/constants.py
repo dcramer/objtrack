e1013604928693baca4ba8941b3c46f77d7bc87b
David Cramer <dcramer@gmail.com>
2009-01-28 07:34:46 +0000

    Complete rewrite of object tracking. Session based storage only.

--- django_object_view_tracking/constants.py
+++
@@ -1,5 +0,0 @@
-OBJECT_TRACKING_SESSION_KEY     = 'object_tracking'
-# You could create your own attribute if you want to use this across different
-# types of models
-OBJECT_TRACKING_DATE_ATTRIBUTE  = 'date_changed'
-OBJECT_TRACKING_KEY_SEPARATOR   = ','
--- django_object_view_tracking/models.py
+++ django_object_view_tracking/models.py
@@ -1,23 +1,51 @@
 from django.db import models
-
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 
-from django_object_view_tracking.constants import *
-from django_object_view_tracking.manager import *
+import datetime
 
-class ObjectTracker(models.Model, ObjectTrackerHandler):
-    user            = models.ForeignKey(User)
-    content_type    = models.ForeignKey(ContentType)
-    date            = models.DateTimeField(blank=True, null=True)
-    _objects        = models.TextField(db_column='objects')
-
-    objects         = ObjectTrackerManager()
-
-    def _get_objects(self):
-        return self._objects.split(OBJECT_TRACKING_KEY_SEPARATOR)
+class ObjectTracker(object):
+    """
+    tracker = ObjectTracker(request.session)
+    tracker.has_viewed(instance, 'date_changed_field_name')
+    """
     
-    def _set_objects(self, values):
-        self._objects = OBJECT_TRACKING_KEY_SEPARATOR.join(values)
+    key_name = 'objtracker'
     
-    instances = property(_get_objects, _set_objects)
+    def __init__(self, session):
+        self.session = session
+    
+    def mark_as_viewed(self, instance, commit=True):
+        ct = ContentType.objects.get_for_model(instance.__class__).id
+        
+        ts = datetime.datetime.now()
+        
+        self.session.setdefault(self.key_name, {'_date': ts}).setdefault(ct, {})[instance.pk] = ts
+
+    def mark_all_as_viewed(self, commit=True):
+        self.session[self.key_name] = {'_date': datetime.datetime.now()}
+
+    def has_viewed_raw(self, model_class, pk, date_value=None):
+        session = self.session.get(self.key_name)
+        if not session:
+            return False
+            
+        last_date = self.session[self.key_name].get('_date', None)
+        
+        ct = ContentType.objects.get_for_model(model_class).id
+        if ct not in session:
+            if last_date and date_value < last_date:
+                return True
+            return False
+        
+        if not date_value or not last_date:
+            return False
+        
+        date = session[ct].get(pk, last_date)
+        return date_value < date
+
+    def has_viewed(self, instance, date_attr=None):
+        if date_attr:
+            date_value = getattr(instance, date_attr)
+        else:
+            date_value = None
+        return self.has_viewed_raw(instance.__class__, instance.pk, date_value)
