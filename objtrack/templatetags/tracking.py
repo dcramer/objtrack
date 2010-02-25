bae1cb1270cc3322db727b0735b00660aad64083
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-02-24 18:24:47 -0600

    Fix order of args

--- objtrack/templatetags/tracking.py
+++ objtrack/templatetags/tracking.py
@@ -8,7 +8,7 @@ except ImportError:
 register = template.Library()
 
 @register.filter()
-def with_tracking(session, queryset, date_field):
+def with_tracking(queryset, session, date_field):
     tracker = ObjectTracker(session)
     for obj in queryset:
         yield obj, tracker.has_viewed(obj, date_field)
